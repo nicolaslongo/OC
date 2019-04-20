aa255570ec124f6d812e571bdbb53207ff87d8ec
Rodrigo Etchegaray <rorroeche@gmail.com>
2019-04-20 12:11:21 -0300

    Listo lo de MIPS y ya agregado al informe. Faltan las otras correciones del informe

--- tp0/dos2unix.s
+++ tp0/dos2unix.s
@@ -68,24 +68,20 @@ $L19:
 	lw	$a0,48($fp)
 	la	$t9,fgetc
 	jal	$ra,$t9
-	sb	$v0,24($fp)
-	lbu	$v0,24($fp)
-	sll	$v0,$v0,24
-	sra	$v1,$v0,24
+	sw	$v0,24($fp)
+	lw	$v1,24($fp)
 	li	$v0,-1			# 0xffffffffffffffff
 	bne	$v1,$v0,$L21
 	b	$L18
 $L21:
-	lb	$v1,24($fp)
-	lb	$v0,CR
-	bne	$v1,$v0,$L22
+	lb	$v1,CR
+	lw	$v0,24($fp)
+	bne	$v0,$v1,$L22
 	lw	$a0,48($fp)
 	la	$t9,fgetc
 	jal	$ra,$t9
-	sb	$v0,25($fp)
-	lbu	$v0,25($fp)
-	sll	$v0,$v0,24
-	sra	$v1,$v0,24
+	sw	$v0,28($fp)
+	lw	$v1,28($fp)
 	li	$v0,-1			# 0xffffffffffffffff
 	bne	$v1,$v0,$L23
 	lb	$v0,CR
@@ -95,9 +91,9 @@ $L21:
 	jal	$ra,$t9
 	b	$L18
 $L23:
-	lb	$v1,25($fp)
-	lb	$v0,LF
-	bne	$v1,$v0,$L25
+	lb	$v1,LF
+	lw	$v0,28($fp)
+	bne	$v0,$v1,$L25
 	lb	$v0,LF
 	move	$a0,$v0
 	lw	$a1,52($fp)
@@ -110,15 +106,13 @@ $L25:
 	lw	$a1,52($fp)
 	la	$t9,fputc
 	jal	$ra,$t9
-	lb	$v0,25($fp)
-	move	$a0,$v0
+	lw	$a0,28($fp)
 	lw	$a1,52($fp)
 	la	$t9,fputc
 	jal	$ra,$t9
 	b	$L19
 $L22:
-	lb	$v0,24($fp)
-	move	$a0,$v0
+	lw	$a0,24($fp)
 	lw	$a1,52($fp)
 	la	$t9,fputc
 	jal	$ra,$t9
@@ -137,18 +131,21 @@ $LC1:
 	.ascii	"-i\000"
 	.align	2
 $LC2:
-	.ascii	"-o\000"
+	.ascii	"-\000"
 	.align	2
 $LC3:
-	.ascii	"rt\000"
+	.ascii	"-o\000"
 	.align	2
 $LC4:
-	.ascii	"No se pudo abrir el archivo de entrada\n\000"
+	.ascii	"rt\000"
 	.align	2
 $LC5:
-	.ascii	"wt\000"
+	.ascii	"No se pudo abrir el archivo de entrada\n\000"
 	.align	2
 $LC6:
+	.ascii	"wt\000"
+	.align	2
+$LC7:
 	.ascii	"No se pudo abrir el archivo de salida\n\000"
 	.text
 	.align	2
@@ -201,44 +198,65 @@ $L29:
 	bne	$v0,$zero,$L32
 	lw	$v0,76($fp)
 	addu	$v0,$v0,8
-	lw	$v0,0($v0)
-	sw	$v0,32($fp)
-	li	$v0,1			# 0x1
-	sw	$v0,24($fp)
-	b	$L33
-$L32:
-	lw	$v0,76($fp)
-	addu	$v0,$v0,4
 	lw	$a0,0($v0)
 	la	$a1,$LC2
 	la	$t9,strcmp
 	jal	$ra,$t9
-	bne	$v0,$zero,$L34
+	beq	$v0,$zero,$L34
+	lw	$v0,76($fp)
+	addu	$v0,$v0,8
+	lw	$v0,0($v0)
+	sw	$v0,32($fp)
+	li	$v0,1			# 0x1
+	sw	$v0,24($fp)
+	b	$L34
+$L32:
+	lw	$v0,76($fp)
+	addu	$v0,$v0,4
+	lw	$a0,0($v0)
+	la	$a1,$LC3
+	la	$t9,strcmp
+	jal	$ra,$t9
+	bne	$v0,$zero,$L35
+	lw	$v0,76($fp)
+	addu	$v0,$v0,8
+	lw	$a0,0($v0)
+	la	$a1,$LC2
+	la	$t9,strcmp
+	jal	$ra,$t9
+	beq	$v0,$zero,$L34
 	lw	$v0,76($fp)
 	addu	$v0,$v0,8
 	lw	$v0,0($v0)
 	sw	$v0,36($fp)
 	li	$v0,1			# 0x1
 	sw	$v0,28($fp)
-	b	$L33
-$L34:
+	b	$L34
+$L35:
 	la	$t9,mostrarMensajeErrorParametrosInvalidos
 	jal	$ra,$t9
 	sw	$v0,48($fp)
 	b	$L28
-$L33:
+$L34:
 	lw	$v0,72($fp)
 	slt	$v0,$v0,5
 	bne	$v0,$zero,$L31
 	lw	$v0,24($fp)
-	beq	$v0,$zero,$L37
+	beq	$v0,$zero,$L39
 	lw	$v0,76($fp)
 	addu	$v0,$v0,12
+	lw	$a0,0($v0)
+	la	$a1,$LC3
+	la	$t9,strcmp
+	jal	$ra,$t9
+	bne	$v0,$zero,$L40
+	lw	$v0,76($fp)
+	addu	$v0,$v0,16
 	lw	$a0,0($v0)
 	la	$a1,$LC2
 	la	$t9,strcmp
 	jal	$ra,$t9
-	bne	$v0,$zero,$L38
+	beq	$v0,$zero,$L31
 	lw	$v0,76($fp)
 	addu	$v0,$v0,16
 	lw	$v0,0($v0)
@@ -246,12 +264,12 @@ $L33:
 	li	$v0,1			# 0x1
 	sw	$v0,28($fp)
 	b	$L31
-$L38:
+$L40:
 	la	$t9,mostrarMensajeErrorParametrosInvalidos
 	jal	$ra,$t9
 	sw	$v0,48($fp)
 	b	$L28
-$L37:
+$L39:
 	lw	$v0,28($fp)
 	beq	$v0,$zero,$L31
 	lw	$v0,76($fp)
@@ -260,7 +278,14 @@ $L37:
 	la	$a1,$LC1
 	la	$t9,strcmp
 	jal	$ra,$t9
-	bne	$v0,$zero,$L42
+	bne	$v0,$zero,$L45
+	lw	$v0,76($fp)
+	addu	$v0,$v0,16
+	lw	$a0,0($v0)
+	la	$a1,$LC2
+	la	$t9,strcmp
+	jal	$ra,$t9
+	beq	$v0,$zero,$L31
 	lw	$v0,76($fp)
 	addu	$v0,$v0,16
 	lw	$v0,0($v0)
@@ -268,52 +293,52 @@ $L37:
 	li	$v0,1			# 0x1
 	sw	$v0,24($fp)
 	b	$L31
-$L42:
+$L45:
 	la	$t9,mostrarMensajeErrorParametrosInvalidos
 	jal	$ra,$t9
 	sw	$v0,48($fp)
 	b	$L28
 $L31:
 	lw	$v0,32($fp)
-	beq	$v0,$zero,$L44
+	beq	$v0,$zero,$L48
 	lw	$a0,32($fp)
-	la	$a1,$LC3
+	la	$a1,$LC4
 	la	$t9,fopen
 	jal	$ra,$t9
 	sw	$v0,40($fp)
 	lw	$v0,40($fp)
-	bne	$v0,$zero,$L46
+	bne	$v0,$zero,$L50
 	la	$a0,__sF+176
-	la	$a1,$LC4
+	la	$a1,$LC5
 	la	$t9,fprintf
 	jal	$ra,$t9
 	li	$v0,-1			# 0xffffffffffffffff
 	sw	$v0,48($fp)
 	b	$L28
-$L44:
+$L48:
 	la	$v0,__sF
 	sw	$v0,40($fp)
-$L46:
+$L50:
 	lw	$v0,36($fp)
-	beq	$v0,$zero,$L47
+	beq	$v0,$zero,$L51
 	lw	$a0,36($fp)
-	la	$a1,$LC5
+	la	$a1,$LC6
 	la	$t9,fopen
 	jal	$ra,$t9
 	sw	$v0,44($fp)
 	lw	$v0,44($fp)
-	bne	$v0,$zero,$L49
+	bne	$v0,$zero,$L53
 	la	$a0,__sF+176
-	la	$a1,$LC6
+	la	$a1,$LC7
 	la	$t9,fprintf
 	jal	$ra,$t9
 	li	$v0,-1			# 0xffffffffffffffff
 	sw	$v0,48($fp)
 	b	$L28
-$L47:
+$L51:
 	la	$v0,__sF+88
 	sw	$v0,44($fp)
-$L49:
+$L53:
 	lw	$a0,40($fp)
 	lw	$a1,44($fp)
 	la	$t9,windowsToLinux
--- tp0/unix2dos.s
+++ tp0/unix2dos.s
@@ -68,17 +68,15 @@ $L19:
 	lw	$a0,48($fp)
 	la	$t9,fgetc
 	jal	$ra,$t9
-	sb	$v0,24($fp)
-	lbu	$v0,24($fp)
-	sll	$v0,$v0,24
-	sra	$v1,$v0,24
+	sw	$v0,24($fp)
+	lw	$v1,24($fp)
 	li	$v0,-1			# 0xffffffffffffffff
 	bne	$v1,$v0,$L21
 	b	$L18
 $L21:
-	lb	$v1,24($fp)
-	lb	$v0,LF
-	bne	$v1,$v0,$L22
+	lb	$v1,LF
+	lw	$v0,24($fp)
+	bne	$v0,$v1,$L22
 	lb	$v0,CR
 	move	$a0,$v0
 	lw	$a1,52($fp)
@@ -91,8 +89,7 @@ $L21:
 	jal	$ra,$t9
 	b	$L19
 $L22:
-	lb	$v0,24($fp)
-	move	$a0,$v0
+	lw	$a0,24($fp)
 	lw	$a1,52($fp)
 	la	$t9,fputc
 	jal	$ra,$t9
@@ -111,18 +108,21 @@ $LC1:
 	.ascii	"-i\000"
 	.align	2
 $LC2:
-	.ascii	"-o\000"
+	.ascii	"-\000"
 	.align	2
 $LC3:
-	.ascii	"rt\000"
+	.ascii	"-o\000"
 	.align	2
 $LC4:
-	.ascii	"No se pudo abrir el archivo de entrada\n\000"
+	.ascii	"rt\000"
 	.align	2
 $LC5:
-	.ascii	"wt\000"
+	.ascii	"No se pudo abrir el archivo de entrada\n\000"
 	.align	2
 $LC6:
+	.ascii	"wt\000"
+	.align	2
+$LC7:
 	.ascii	"No se pudo abrir el archivo de salida\n\000"
 	.text
 	.align	2
@@ -175,44 +175,65 @@ $L25:
 	bne	$v0,$zero,$L28
 	lw	$v0,76($fp)
 	addu	$v0,$v0,8
-	lw	$v0,0($v0)
-	sw	$v0,32($fp)
-	li	$v0,1			# 0x1
-	sw	$v0,24($fp)
-	b	$L29
-$L28:
-	lw	$v0,76($fp)
-	addu	$v0,$v0,4
 	lw	$a0,0($v0)
 	la	$a1,$LC2
 	la	$t9,strcmp
 	jal	$ra,$t9
-	bne	$v0,$zero,$L30
+	beq	$v0,$zero,$L30
+	lw	$v0,76($fp)
+	addu	$v0,$v0,8
+	lw	$v0,0($v0)
+	sw	$v0,32($fp)
+	li	$v0,1			# 0x1
+	sw	$v0,24($fp)
+	b	$L30
+$L28:
+	lw	$v0,76($fp)
+	addu	$v0,$v0,4
+	lw	$a0,0($v0)
+	la	$a1,$LC3
+	la	$t9,strcmp
+	jal	$ra,$t9
+	bne	$v0,$zero,$L31
+	lw	$v0,76($fp)
+	addu	$v0,$v0,8
+	lw	$a0,0($v0)
+	la	$a1,$LC2
+	la	$t9,strcmp
+	jal	$ra,$t9
+	beq	$v0,$zero,$L30
 	lw	$v0,76($fp)
 	addu	$v0,$v0,8
 	lw	$v0,0($v0)
 	sw	$v0,36($fp)
 	li	$v0,1			# 0x1
 	sw	$v0,28($fp)
-	b	$L29
-$L30:
+	b	$L30
+$L31:
 	la	$t9,mostrarMensajeErrorParametrosInvalidos
 	jal	$ra,$t9
 	sw	$v0,48($fp)
 	b	$L24
-$L29:
+$L30:
 	lw	$v0,72($fp)
 	slt	$v0,$v0,5
 	bne	$v0,$zero,$L27
 	lw	$v0,24($fp)
-	beq	$v0,$zero,$L33
+	beq	$v0,$zero,$L35
 	lw	$v0,76($fp)
 	addu	$v0,$v0,12
+	lw	$a0,0($v0)
+	la	$a1,$LC3
+	la	$t9,strcmp
+	jal	$ra,$t9
+	bne	$v0,$zero,$L36
+	lw	$v0,76($fp)
+	addu	$v0,$v0,16
 	lw	$a0,0($v0)
 	la	$a1,$LC2
 	la	$t9,strcmp
 	jal	$ra,$t9
-	bne	$v0,$zero,$L34
+	beq	$v0,$zero,$L27
 	lw	$v0,76($fp)
 	addu	$v0,$v0,16
 	lw	$v0,0($v0)
@@ -220,12 +241,12 @@ $L29:
 	li	$v0,1			# 0x1
 	sw	$v0,28($fp)
 	b	$L27
-$L34:
+$L36:
 	la	$t9,mostrarMensajeErrorParametrosInvalidos
 	jal	$ra,$t9
 	sw	$v0,48($fp)
 	b	$L24
-$L33:
+$L35:
 	lw	$v0,28($fp)
 	beq	$v0,$zero,$L27
 	lw	$v0,76($fp)
@@ -234,7 +255,14 @@ $L33:
 	la	$a1,$LC1
 	la	$t9,strcmp
 	jal	$ra,$t9
-	bne	$v0,$zero,$L38
+	bne	$v0,$zero,$L41
+	lw	$v0,76($fp)
+	addu	$v0,$v0,16
+	lw	$a0,0($v0)
+	la	$a1,$LC2
+	la	$t9,strcmp
+	jal	$ra,$t9
+	beq	$v0,$zero,$L27
 	lw	$v0,76($fp)
 	addu	$v0,$v0,16
 	lw	$v0,0($v0)
@@ -242,52 +270,52 @@ $L33:
 	li	$v0,1			# 0x1
 	sw	$v0,24($fp)
 	b	$L27
-$L38:
+$L41:
 	la	$t9,mostrarMensajeErrorParametrosInvalidos
 	jal	$ra,$t9
 	sw	$v0,48($fp)
 	b	$L24
 $L27:
 	lw	$v0,32($fp)
-	beq	$v0,$zero,$L40
+	beq	$v0,$zero,$L44
 	lw	$a0,32($fp)
-	la	$a1,$LC3
+	la	$a1,$LC4
 	la	$t9,fopen
 	jal	$ra,$t9
 	sw	$v0,40($fp)
 	lw	$v0,40($fp)
-	bne	$v0,$zero,$L42
+	bne	$v0,$zero,$L46
 	la	$a0,__sF+176
-	la	$a1,$LC4
+	la	$a1,$LC5
 	la	$t9,fprintf
 	jal	$ra,$t9
 	li	$v0,-1			# 0xffffffffffffffff
 	sw	$v0,48($fp)
 	b	$L24
-$L40:
+$L44:
 	la	$v0,__sF
 	sw	$v0,40($fp)
-$L42:
+$L46:
 	lw	$v0,36($fp)
-	beq	$v0,$zero,$L43
+	beq	$v0,$zero,$L47
 	lw	$a0,36($fp)
-	la	$a1,$LC5
+	la	$a1,$LC6
 	la	$t9,fopen
 	jal	$ra,$t9
 	sw	$v0,44($fp)
 	lw	$v0,44($fp)
-	bne	$v0,$zero,$L45
+	bne	$v0,$zero,$L49
 	la	$a0,__sF+176
-	la	$a1,$LC6
+	la	$a1,$LC7
 	la	$t9,fprintf
 	jal	$ra,$t9
 	li	$v0,-1			# 0xffffffffffffffff
 	sw	$v0,48($fp)
 	b	$L24
-$L43:
+$L47:
 	la	$v0,__sF+88
 	sw	$v0,44($fp)
-$L45:
+$L49:
 	lw	$a0,40($fp)
 	lw	$a1,44($fp)
 	la	$t9,linuxToWindows
